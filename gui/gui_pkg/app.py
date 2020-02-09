e07bbac891ab33a8a444e2241b3fc9a14e1305d0
System Administrator <root@Brandons-MacBook-Pro.local>
2020-02-09 02:06:21 -0500

    update:

--- gui/gui_pkg/app.py
+++ gui/gui_pkg/app.py
@@ -10,7 +10,7 @@ from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg as NavigationToolbar
 #import wx.lib.agw.multidirdialog as MDD
 import wx.lib.inspection
-
+from read_data import read_data
 
 class MainApp(wx.App):
     
@@ -171,7 +171,7 @@ class MainPanel(wx.Panel):
         controlSizer.Add(self.stats, 0, wx.ALL|wx.EXPAND, 0)
         #Add waveform and charge spectrum buttons
         spectrumsizer=wx.BoxSizer(wx.HORIZONTAL)
-        waveformspec=wx.Button(self, id=wx.ID_ANY, label="Waveforms")
+        waveformspec=wx.Button(self, id=wx.ID_ANY, label="Amplitudes")
         self.Bind(wx.EVT_BUTTON, self.plotwaveformspec, waveformspec)
         chargespec=wx.Button(self, id=wx.ID_ANY, label="Charges")
         self.Bind(wx.EVT_BUTTON, self.plotchargespec, chargespec)
@@ -248,19 +248,20 @@ class MainPanel(wx.Panel):
 
     def next(self, event):
         try:
-            self.risetimeval.SetLabel(str(self.data.risetimes[self.index + 1]))
+           # self.risetimeval.SetLabel(str(self.data.risetimes[self.index + 1]))
             self.index += 1
         except IndexError:
             return
-        self.peaktimeval.SetLabel(str(self.data.peaktimes[self.index]))
-        self.falltimeval.SetLabel(str(self.data.falltimes[self.index]))
-        self.chargeval.SetLabel(str(self.data.charges[self.index])) 
+        #self.peaktimeval.SetLabel(str(self.data.peaktimes[self.index]))
+        #self.falltimeval.SetLabel(str(self.data.falltimes[self.index]))
+        #self.chargeval.SetLabel(str(self.data.charges[self.index])) 
         #Check to see if current page is last one, if it is then make the next one
         if self.plotter.nb.GetSelection() == self.plotter.nb.GetPageCount() -1:
             axes = self.plotter.add('Waveform {}'.format(self.index + 1)).gca()
-            axes.plot(self.data.set[self.index][0], self.data.set[self.index][1])
-            axes.plot([self.data.risetimes[self.index], self.data.risetimes[self.index]], [0, 4], color="Red")
-            axes.plot([self.data.falltimes[self.index], self.data.falltimes[self.index]],[0,4], color="Red")
+            axes.plot(self.data.events[self.index + 2].event[0][1], self.data.events[self.index + 2].event[0][0]) #It appears that every other "event" is blank... why?!?!?
+           # axes.plot([self.data.risetimes[self.index], self.data.risetimes[self.index]], [0, 4], color="Red")
+           # axes.plot([self.data.falltimes[self.index], self.data.falltimes[self.index]],[0,4], color="Red")
+            print(self.data.events[self.index + 1].event[0][0])
             axes.set_title("Voltage vs Time")
             axes.set_ylabel("Voltage (V)")
             axes.set_xlabel("Time (ns)")
@@ -304,7 +305,12 @@ class MainPanel(wx.Panel):
 
 
 class Data:
-    def __init__(self):
+    def __init__(self, f = "Trial 21.dat"):
+        data = read_data(f)
+        self.events=data.events
+
+
+
         self.set=[[[1,2,3],[2,1,4]],[[1, 2, 3, 4, 5], [2, 1, 4, 2, 3]],[[1, 4, 6, 19], [4, 7, 8, 15]], [[1,3,5,5],[-3,5,-7,10]]]
         self.risetimes = [2,3,4,5]
         self.peaktimes = [4,7,9,10]
