9bf97ead2afa897687934c98b5ea71ee66a28c29
System Administrator <root@Brandons-MacBook-Pro.local>
2020-03-01 02:07:01 -0500

    update

--- gui/gui_pkg/stats.py
+++ gui/gui_pkg/stats.py
@@ -1,12 +1,15 @@
 #stats file 
 
+    
+from scipy import integrate
 import numpy as np
 import pandas as pd
 
 
 class Stats:
-    def __init__(series):
+    def __init__(self,series):
         self.series = series
+        self._peak_time = None
 
     @staticmethod
     def moving_avg(series):
@@ -14,22 +17,38 @@ class Stats:
 
     @property
     def peak(self):
-        if self._peak:
-            return self._peak
-        self._peak = self.series.max()
+        pass
+        # if not self._peak:
+        #     self._peak = self.series.max()
+        # return self._peak
+    
+    def charge(self):
+        return self.series.integrate()/50
+
+    def amplitude(self):
+        return self.series[self.peak_time()]
 
     def peak_time(self):
-        return self.series.idxmax()
+        if not self._peak_time:
+            self._peak_time = self.series.idxmax()
+        return self._peak_time
+
+    # def rise_time(self):
+
 
 def process_single_channel_per_event_channel(time_series, processors):
     for processor in processors:
-        print(Stats.__dict__[processor].__get__(Stats()))
-        time_series = Stats.__dict__[processor].__get__(Stats())(time_series)
+        time_series = Stats.__dict__[processor].__get__(Stats([]))(time_series)
     
     return time_series
     
 
-def calc_stats_single_channel(event_series, processors = ['moving_avg'], stats = ['peak_time']):
+# Need to add baseline adjust
+def calc_stats_single_channel(
+    event_series,
+    processors = ['moving_avg'],
+    stats = ['peak_time', 'amplitude']
+):
     
     new_events = event_series.map(
         lambda event: event.map(
@@ -37,93 +56,50 @@ def calc_stats_single_channel(event_series, processors = ['moving_avg'], stats =
         )
     )
 
-    stat_dict = [ {
+    stat_dict = pd.Series([ {
         stat: [] for stat in stats
-    } for channel in event_series[0]]
+    } for channel in event_series[0]], index=event_series[0].index)
     for event in new_events:
         for channel, channel_event in event.items():
             for stat in stats:
                 stat_calculator = Stats(channel_event)
                 stat_dict[channel][stat].append(
-                    stat_calculator.__getattribute__(stat)
+                    stat_calculator.__getattribute__(stat)()
                 )
+
+    for channel, channel_event in event.items():
+        for stat in stats:
+            stat_dict[channel][stat] = pd.Series(stat_dict[channel][stat])
     
     return stat_dict
-            
 
 
-# class EventStats(object):
-#     def __init__(self, event, processors = [moving_avg]):
-#         for processor in processors:
-#             event = processor(event)
-#         self.event = event
-#         single_channel
+def integrate_method(self, how='trapz', unit='s'):
+    '''Numerically integrate the time series.
 
-#     def rise_time():
-        
-#         for i = 0, i < N_BINS - 2, i ++:
-#                 if ((channel_waveform(i) < PEAK_THRESHOLD)
-#                     and channel_waveform(i+1) >= PEAK_THRESHOLD):
-                    
-#                     return (
-#                         (PEAK_THRESHOLD - channe_waveform(i))
-#                         / (channel_waveform(i + 1) - channel_waveform(i))
-#                         * (channel_time(i+1) -channel_time(i))
-#                         ) + channel_time(i)
-                
-#             return std global numeric_limits
+    @param how: the method to use (trapz by default)
+    @return 
+
+    Available methods:
+     * trapz - trapezoidal
+     * cumtrapz - cumulative trapezoidal
+     * simps - Simpson's rule
+     * romb - Romberger's rule
+
+    See http://docs.scipy.org/doc/scipy/reference/integrate.html for the method details.
+    or the source code
+    https://github.com/scipy/scipy/blob/master/scipy/integrate/quadrature.py
+    '''
+    available_rules = set(['trapz', 'cumtrapz', 'simps', 'romb'])
+    if how in available_rules:
+        rule = integrate.__getattribute__(how)
+    else:
+        print('Unsupported integration rule: %s' % (how))
+        print('Expecting one of these sample-based integration rules: %s' % (str(list(available_rules))))
+        raise AttributeError
     
-# # assumes curve has been smoothed.
+    result = rule(self.values, self.index.astype(np.int64) / 10**9)
+    #result = rule(self.values)
+    return result
 
-# def peak_time(wave(N_BINS), time(N_BINS)):
-    
-#     max_dertime = 0
-#     max_derivative = 0
-    
-#     def derivatives(N_BINS):
-#         for i in range(0, N_BINS - 1, i ++)
-#             derivatives(i) = (wave(i + 1) - wave(i)) / (time(i + 1) - time(i))
-    
-#     if ( i >= 9 ):
-        
-#         def avg_derivative = 0
-#         for (j = 0, j < 10, j++):
-#             avg_derivative += derivatives(i - j)
-#         if (max_derivative < avg_derivative):
-#             max_derivative = avg_derivative
-#             max_dertime = time(i - 4)
-
-#             return max_dertime 
-
-
-# def amplitude(channel_waveform(N_BINS)):
-
-#         # find baseline for channel to get amplitude for that channel         sum_ = 0.0
-#         for i in range(0, i < 10, i++):
-
-#             sum_ += channel_waveform(i)
-            
-#         baseline = sum_/10
-
-#         # find amplitude for channel
-
-#         max_ = -10000.0
-#         for i in range(0, i < N_BINS - 2, i++):
-#             if (channel_waveform(i) > max_):
-#                 max_ = channel_waveform(i)
-
-
-#         return max_
-
-# def charge(channel_waveform(N_BINS), channel_time(N_BINS)):
-
-#     # performs Reimann sum
-
-#     sum_ = 0.0
-#     for i in range(0, i < N_BINS - 2, i++):
-#         sum_ += (channel_waveform(i) + channel_waveform(i + 1)) * (channel_time(i) + channel_time(i + 1)) / 2
-
-#     return sum_ / RESISTANCE 
-
-
-    
+pd.Series.integrate = integrate_method
